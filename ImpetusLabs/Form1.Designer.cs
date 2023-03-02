3008e830dcc51124a523149ae3ce41bfb9d9fd73
Gustavo Serrano <gjserranom@gmail.com>
2023-03-02 15:54:48 -0400

    Add project files.

---
+++ ImpetusLabs/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace ImpetusLabs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnLab01Start = new System.Windows.Forms.Button();
+            this.BtnLab01Stop = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Lbl2Lab01Test1 = new System.Windows.Forms.Label();
+            this.LblLab01Test1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnLab01Start
+            // 
+            this.BtnLab01Start.Location = new System.Drawing.Point(514, 492);
+            this.BtnLab01Start.Name = "BtnLab01Start";
+            this.BtnLab01Start.Size = new System.Drawing.Size(225, 138);
+            this.BtnLab01Start.TabIndex = 0;
+            this.BtnLab01Start.Text = "START SIM";
+            this.BtnLab01Start.UseVisualStyleBackColor = true;
+            this.BtnLab01Start.Click += new System.EventHandler(this.BtnLab01Start_Click_1);
+            // 
+            // BtnLab01Stop
+            // 
+            this.BtnLab01Stop.Location = new System.Drawing.Point(514, 492);
+            this.BtnLab01Stop.Name = "BtnLab01Stop";
+            this.BtnLab01Stop.Size = new System.Drawing.Size(225, 138);
+            this.BtnLab01Stop.TabIndex = 1;
+            this.BtnLab01Stop.Text = "STOP SIM";
+            this.BtnLab01Stop.UseVisualStyleBackColor = true;
+            this.BtnLab01Stop.Click += new System.EventHandler(this.BtnLab01Stop_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.Lbl2Lab01Test1);
+            this.panel1.Controls.Add(this.LblLab01Test1);
+            this.panel1.Location = new System.Drawing.Point(63, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(676, 297);
+            this.panel1.TabIndex = 2;
+            // 
+            // Lbl2Lab01Test1
+            // 
+            this.Lbl2Lab01Test1.BackColor = System.Drawing.Color.Silver;
+            this.Lbl2Lab01Test1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbl2Lab01Test1.Location = new System.Drawing.Point(100, 35);
+            this.Lbl2Lab01Test1.Name = "Lbl2Lab01Test1";
+            this.Lbl2Lab01Test1.Size = new System.Drawing.Size(73, 33);
+            this.Lbl2Lab01Test1.TabIndex = 1;
+            this.Lbl2Lab01Test1.Text = "NOT RUN";
+            this.Lbl2Lab01Test1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Lbl2Lab01Test1.Click += new System.EventHandler(this.Lbl2Lab01Test1_Click);
+            // 
+            // LblLab01Test1
+            // 
+            this.LblLab01Test1.Location = new System.Drawing.Point(35, 42);
+            this.LblLab01Test1.Name = "LblLab01Test1";
+            this.LblLab01Test1.Size = new System.Drawing.Size(59, 19);
+            this.LblLab01Test1.TabIndex = 0;
+            this.LblLab01Test1.Text = "Test 1";
+            this.LblLab01Test1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 438);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 172);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnLab01Stop);
+            this.Controls.Add(this.BtnLab01Start);
+            this.Name = "Form1";
+            this.Text = "Hey";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnLab01Start;
+        private System.Windows.Forms.Button BtnLab01Stop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Lbl2Lab01Test1;
+        private System.Windows.Forms.Label LblLab01Test1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ImpetusLabs/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Opc.UaFx.Client;
+
+namespace ImpetusLabs
+{
+    public partial class Form1 : Form
+    {
+        OpcClient client = new OpcClient("opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1");
+        
+        public Form1()
+        {
+            InitializeComponent();
+            BtnLab01Stop.Visible = false;
+            Lbl2Lab01Test1.BackColor = Color.Silver;
+        }
+
+        private void btnOpcWrite_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fuck The Police");
+
+            var tagName = "ns=2;s=::[GustavoDevice]Program:SIMULATION.BIT";
+
+            client.Connect();
+
+            string temperature;
+            
+            //temperature = Convert.ToString(txtOpcDataWrite.Text);
+
+            //client.WriteNode(tagName, temperature);
+            client.Disconnect();
+            
+        }
+
+        private void btnOpcRead_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Fuck The Police");
+            //string opcUrl = "opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1";
+            var tagName = "ns=2;s=::[GustavoDevice]Program:SIMULATION.BIT";
+            //var client = new OpcClient(opcUrl);
+            client.Connect();
+            var temperature = client.ReadNode(tagName);
+
+            //txtOpcDataRead.Text = temperature.ToString();
+        }
+
+        private void BtnLab01Start_Click_1(object sender, EventArgs e)
+        {
+            //string opcUrl = "opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1";
+            var tagName = "ns=2;s=::[GustavoDevice]Program:SIMULATION.BIT";
+            //var client = new OpcClient(opcUrl);
+            client.Connect();
+
+            client.WriteNode(tagName, true);
+            BtnLab01Start.Visible = false;
+            BtnLab01Stop.Visible = true;
+
+            client.Disconnect();
+        }
+
+        private void BtnLab01Stop_Click(object sender, EventArgs e)
+        {
+            //string opcUrl = "opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1";
+            var tagName = "ns=2;s=::[GustavoDevice]Program:SIMULATION.BIT";
+            //var client = new OpcClient(opcUrl);
+            client.Connect();
+
+            client.WriteNode(tagName, false);
+            BtnLab01Start.Visible = true;
+            BtnLab01Stop.Visible = false;
+            client.Disconnect();
+        }
+
+        public void Lbl2Lab01Test1_Click(object sender, EventArgs e)
+        {
+
+            //string opcUrl = "opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1";
+            var tagName = "ns=2;s=[GustavoDevice]LAB01.VAR[1]";
+            //var client = new OpcClient(opcUrl);
+            client.Connect();
+            var Lab01Test1 = client.ReadNode(tagName);
+            if (Lab01Test1.Equals(1))
+            {
+                LblLab01Test1.BackColor = Color.Green;
+            }
+            if (Lab01Test1.Equals(-1))
+            {
+                LblLab01Test1.BackColor = Color.Red;
+            }
+            client.Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string opcUrl = "opc.tcp://192.168.4.44:4990/FactoryTalkLinxGateway1";
+            var tagName = "ns=2;s=[GustavoDevice]LAB01.VAR[2]";
+            //var client = new OpcClient(opcUrl);
+            client.Connect();
+            var Lab01Test1 = client.ReadNode(tagName);
+            if (Lab01Test1.ToString().Equals("1"))
+            {
+                Lbl2Lab01Test1.BackColor = Color.Green;
+                Lbl2Lab01Test1.Text = "PASSED";
+            }
+            if (Lab01Test1.ToString().Equals("-1"))
+            {
+                Lbl2Lab01Test1.BackColor = Color.Red;
+                Lbl2Lab01Test1.Text = "FAILED";
+            }
+            client.Disconnect();
+        }
+    }
+}
